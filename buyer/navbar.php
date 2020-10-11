8658fbf759ae38bc25fadb548f88dff0919a4799
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2020-10-11 11:14:55 +0530

    commit

--- buyer/navbar.php
+++ buyer/navbar.php
@@ -1,15 +1,16 @@
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport">
 <link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link href="font-awesome.min.css" rel="stylesheet">	
-<link href="animate.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <div>
 <div class="topnav" id="myTopnav">
-  <a href="index.php" class="navlogo"><img src="" alt="" class = "logo">Thoga.lk</a>
+  <a href="index.php" class="navlogo"><img  width=100px src="../imgs/logo/logo thoga.png" alt="" class = "logo"></a>
   <a href="index.php" class="active">Home</a>
   <a href="news.php">News</a>
   <a href="#contact">Contact</a>
