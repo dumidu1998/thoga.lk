18edcb334701bc13204e2909992a0212fe5d75ff
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2020-10-19 19:42:48 +0530

    hhh

--- buyer/index.php
+++ buyer/index.php
@@ -130,23 +130,7 @@ session_start();
 
   
 <script>
-// var modal = document.getElementById("myModal");
-// var btn = document.getElementById("myBtn");
-// var span = document.getElementsByClassName("close")[0];
 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
 
 function closeModal(id) {
   var mod = document.querySelector("#myModal"+id);
