01c3b9ac6b581a9f26b73cc369682ad95acd182b
dumidu1998 <dumiduraj@gmail.com>
2020-10-22 19:42:17 +0530

    forum initial commit

---
+++ Forum/forum.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="forum.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <title>Forum</title>
+</head>
+<body>
+    <div class="container">
+        <div class="grid-item">
+            <div class="filter-topic"> Added by </div>
+            <div class="filter-items"><a href="">Driver</div>
+            <div class="filter-items"><a href="">Farmer</a></div>
+            <div class="filter-items"><a href="">Buyers</a></div><br>
+            <div class="filter-topic"> Sort by </div>
+            <div class="filter-items"><a href="">Date</div>
+            <div class="filter-items"><a href="">Votes</a></div>
+            <!-- <div class="filter-items"><a href="">Buyers</a></div><br> -->
+        </div>
+
+
+        <div class="grid-item">
+           <div class="forum-topic">My problem is my vegetables are not</div>
+
+            
+        </div>
+    </div>
+</body>
+</html>
