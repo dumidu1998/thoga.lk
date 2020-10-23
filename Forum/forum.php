7aaae9883e905a879fd4ea5715fc9951eed0717b
dumidu1998 <dumiduraj@gmail.com>
2020-10-23 12:21:20 +0530

    forum basic done

--- Forum/forum.php
+++ Forum/forum.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="forum.css">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="forum.css" type='text/css'>
+    <link rel="stylesheet" href="font-awesome.min.css" type='text/css'>
+    <link rel="stylesheet" href="font-awesome.css" type='text/css'>
+
     <title>Forum</title>
 </head>
 <body>
     <div class="container">
-        <div class="grid-item">
+        <div class="grid-item0">
             <div class="filter-topic"> Added by </div>
             <div class="filter-items"><a href="">Driver</div>
             <div class="filter-items"><a href="">Farmer</a></div>
@@ -21,10 +23,54 @@
         </div>
 
 
-        <div class="grid-item">
-           <div class="forum-topic">My problem is my vegetables are not</div>
+        <div class="grid-item1">
+            <div class="forum-container">
+                <div class="forum-topic">My Tomato plants are damaged by insects</div>
+                <div class="forum-contols">
+                    <center style="font-size:35px;">0</center>
+                    <center style="font-size:20;">Votes</center><br>
+                    <img src="thumbs-up-solid.svg" height="30px" style="margin-right:25px;margin-left:7px">
+                    <img src="thumbs-down-solid.svg" height="30px" >
+                    <br> Like &nbsp &nbsp &nbsp  Unlike
+                </div>
+                <div class="forum-content">
+                    My Problem is that I have an insect that eats the leaves of my tomato plant. It is a small insect difficult to see using the eye. 
+                    Can someone help me to get rid of that insect. <br>
+                    Thank you. 
+                </div>
+                <div class="reply">
+                    <b>Top Reply</b><br>
+                    <span style="font-size:13px">By - Dumidu Kasun Bandara </span><br>
+                    Cause for this is the mine bug. To remove mine bug you have to spray water mixed with soap untill the patches get healed. 
+                </div>
+            </div>
+
+            <div class="forum-container">
+                <div class="forum-topic">My Tomato plants are damaged by insects</div>
+                <div class="forum-contols">
+                    <center style="font-size:35px;">0</center>
+                    <center style="font-size:20;">Votes</center><br>
+                    <img src="thumbs-up-solid.svg" height="30px" style="margin-right:25px;margin-left:7px">
+                    <img src="thumbs-down-solid.svg" height="30px" >
+                    <br> Like &nbsp &nbsp &nbsp  Unlike
+                </div>
+                <div class="forum-content">
+                    My Problem is that I have an insect that eats the leaves of my tomato plant. It is a small insect difficult to see using the eye. 
+                    Can someone help me to get rid of that insect. <br>
+                    Thank you. 
+                </div>
+                <div class="reply">
+                    <b>Top Reply</b><br>
+                    <span style="font-size:13px">By - Dumidu Kasun Bandara </span><br>
+                    Cause for this is the mine bug. To remove mine bug you have to spray water mixed with soap untill the patches get healed. 
+                </div>
+            </div>
 
             
+        </div>
+        <div >
+            <img class="ad" src="../imgs/ads/a.jpg" alt="ad">
+
         </div>
     </div>
 </body>
