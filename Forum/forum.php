abbf1d700a5c2653a73603ced2cecf077c5dcfa6
dumidu1998 <dumiduraj@gmail.com>
2020-10-27 11:47:45 +0530

    forum home done responsive

--- Forum/forum.php
+++ Forum/forum.php
@@ -6,31 +6,33 @@
     <link rel="stylesheet" href="forum.css" type='text/css'>
     <link rel="stylesheet" href="font-awesome.min.css" type='text/css'>
     <link rel="stylesheet" href="font-awesome.css" type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>Forum</title>
 </head>
-<body>
+<body onload="my()">
     <div class="container">
         <div class="grid-item0">
             <div class="filter-topic"> Added by </div>
-            <div class="filter-items"><a href="">Driver</div>
-            <div class="filter-items"><a href="">Farmer</a></div>
-            <div class="filter-items"><a href="">Buyers</a></div><br>
+            <div class="filter-items"><i class="fa fa-circle" style="font-size:12px;margin-right:10px" aria-hidden="true"></i><a class="links" href="">Driver</a></div>
+            <div class="filter-items"><i class="fa fa-circle" style="font-size:12px;margin-right:10px" aria-hidden="true"></i><a class="links" href="">Farmer</a></div>
+            <div class="filter-items"><i class="fa fa-circle" style="font-size:12px;margin-right:10px" aria-hidden="true"></i><a class="links" href="">Buyers</a></div><br>
+            
             <div class="filter-topic"> Sort by </div>
-            <div class="filter-items"><a href="">Date</div>
-            <div class="filter-items"><a href="">Votes</a></div>
+            <div class="filter-items"><i class="fa fa-circle" style="font-size:12px;margin-right:10px" aria-hidden="true"></i><a class="links" href="">Date</a></div>
+            <div class="filter-items"><i class="fa fa-circle" style="font-size:12px;margin-right:10px" aria-hidden="true"></i><a class="links" href="">Votes</a></div>
             <!-- <div class="filter-items"><a href="">Buyers</a></div><br> -->
         </div>
 
 
         <div class="grid-item1">
             <div class="forum-container">
-                <div class="forum-topic">My Tomato plants are damaged by insects</div>
+                <div class="forum-topic"><a class="links" href="#">My Tomato plants are damaged by insects</a></div>
                 <div class="forum-contols">
                     <center style="font-size:35px;">0</center>
                     <center style="font-size:20;">Votes</center><br>
-                    <img src="thumbs-up-solid.svg" height="30px" style="margin-right:25px;margin-left:7px">
-                    <img src="thumbs-down-solid.svg" height="30px" >
+                    <input type="image" class="like-unlike" id="lbtn" src="thumbs-up-solid.svg" alt="" onclick="dd()" style="margin-right:20px;margin-left:2px" >
+                    <input type="image" class="like-unlike" id="ulbtn" src="thumbs-down-solid.svg" alt="Submit"  onclick="dd()">
                     <br> Like &nbsp &nbsp &nbsp  Unlike
                 </div>
                 <div class="forum-content">
@@ -46,12 +48,12 @@
             </div>
 
             <div class="forum-container">
-                <div class="forum-topic">My Tomato plants are damaged by insects</div>
+                <div class="forum-topic"><a class="links" href="#">My Tomato plants are damaged by insects</a></div>
                 <div class="forum-contols">
                     <center style="font-size:35px;">0</center>
                     <center style="font-size:20;">Votes</center><br>
-                    <img src="thumbs-up-solid.svg" height="30px" style="margin-right:25px;margin-left:7px">
-                    <img src="thumbs-down-solid.svg" height="30px" >
+                    <input type="image" class="like-unlike" id="lbtn" src="thumbs-up-solid.svg" alt="" onclick="dd()" style="margin-right:20px;margin-left:2px" >
+                    <input type="image" class="like-unlike" id="ulbtn" src="thumbs-down-solid.svg" alt="Submit"  onclick="dd()">
                     <br> Like &nbsp &nbsp &nbsp  Unlike
                 </div>
                 <div class="forum-content">
@@ -73,5 +75,19 @@
 
         </div>
     </div>
+
+    <script>
+        function dd(){
+            alert("d");
+        }
+        function my(){
+            vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+            var z=document.querySelectorAll('.like-unlike');
+            var i;
+            if(vw<=600){
+                for(i=0;i<z.length;i++)z[i].disabled=true;
+            }
+        }
+    </script>
 </body>
 </html>
