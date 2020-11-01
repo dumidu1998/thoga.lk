aa77301a0a825c709729a962b6dd50ea8d5d0374
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2020-11-01 11:55:51 +0530

    buyer nav

--- buyer/navbar.php
+++ buyer/navbar.php
@@ -35,6 +35,14 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+// function myFunction() {
+//   var x = document.getElementById("nav-right");
+//   if (x.className === "nav-right") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "nav-right";
+//   }
+// }
 </script>
 
 </body>
