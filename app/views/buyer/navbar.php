88b2d20ad145d89df3be15d3ef6404fa2faf146b
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2020-11-07 11:46:31 +0530

    only languages

--- app/views/buyer/navbar.php
+++ app/views/buyer/navbar.php
@@ -51,7 +51,7 @@ function myFunction() {
 </script>
 <script type="text/javascript">
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+  new google.translate.TranslateElement({pageLanguage: 'en',includedLanguages: 'en,si,ta'}, 'google_translate_element');
 }
 </script>
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
