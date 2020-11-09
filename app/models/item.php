aa3e85c0047ad3c40ed37ddf0c20ba3b31bcd259
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2020-11-09 18:58:06 +0530

    update item model

--- app/models/item.php
+++ app/models/item.php
@@ -3,9 +3,7 @@
 require_once(__DIR__.'/../../core/db_model.php');
 
 class item extends db_model{
-    function get_all(){
-		return $this->read('item',array('*'),null);
-    }
+   
     function get($id){
 
 		return $this->read('item', array('*'), array('id'=>$id));
