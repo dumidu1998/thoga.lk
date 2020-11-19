1a2614a30c23caa27c1098e2151a5562919440fc
dumidu1998 <dumiduraj@gmail.com>
2020-11-19 11:27:38 +0530

    interim

--- app/controller/ForumController.php
+++ app/controller/ForumController.php
@@ -16,24 +16,26 @@ class ForumController{
 
     public function forum(){
         session_start();
+        $out=$this->forum->getposts();
         $view = new View("forum/forum");
+        $view->assign('data',$out);
     }
 
     public function postForum(){
         session_start();
-        $_SESSION['user']=array("user_type"=>"1");
+        // $_SESSION['user']=array("user_type"=>"1");
         print_r($_SESSION['user']);
         if(isset($_POST['post_forum'])){
             $title= $_POST['topic'];
             $description = $_POST['description'];
         
             foreach($_SESSION['user'] as $keys => $values){
-            echo $values['user_type'];
-            $forum_array = array('post' => $title , 'description' => $description, 'category' => $values['user_type']);
+            $forum_array = array('user_id'=>$values['user_id'],'title' => $title , 'description' => $description, 'category' => $values['user_type']);
             }
             $result = $this->forum->insertForum($forum_array);
             print_r($forum_array);
-            header("location: forum");
+            print_r($result);
+            header("location: /thoga.lk/forum");
 
             
         }
