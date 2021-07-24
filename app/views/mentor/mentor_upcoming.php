88d670f4d68abaa43f80ac849cf2021a4ed6355a
Hiruni06 <56539533+Hiruni06@users.noreply.github.com>
2021-07-24 13:16:16 +0530

    done

--- app/views/mentor/mentor_upcoming.php
+++ app/views/mentor/mentor_upcoming.php
@@ -10,95 +10,96 @@
  
 <body>
    <?php 
-  // include  'navbar_dash.php';
+  include  'navbar_dash.php';
    
    ?>
-
-<div class ="split left">
- <h1 class="title">Upcoming Orders</h1>
- 
-<div  style="height:auto;height: 50%;min-height:0px">
-
-<div style="overflow-x:auto;">
-  <table align="center">
-    <tr>
-      <th>Order Id</th>
-      <th>Item Name</th>
-      <th>Pickup Date</th>
-      <th>Total Weight</th>
-      <th>Price</th>
-      <th>Buyer Name</th>
+<div class="containernew">
+      <div class ="split left">
+        <h1 class="title">Upcoming Orders</h1>
       
-      <th>More Details</th>
-      
-      
-    </tr>
+          <div  style="height:auto;height: 50%;min-height:0px">
 
-    
+          <div style="overflow-x:auto;">
+              <table align="center">
+                <tr>
+                  <th>Order Id</th>
+                  <th>Item Name</th>
+                  <th>Pickup Date</th>
+                  <th>Total Weight</th>
+                  <th>Price</th>
+                  <th>Buyer Name</th>
+                  
+                  <th>More Details</th>
+            
+                </tr>
 
-<?php
-if (isset($data[0])){
+          
+
+            <?php
+          // print_r($data);
+            if (isset($data[0])){
 
 
-foreach($data[0] as $key => $values){
-  $itemid= $values['item_id'];
-  $ordid= $values['order_id'];
-  $vegname= $values['vege_name'];
-  $pdate= $values['pickup_date'];
-  $tweight= $values['weight'];
-  $cost= $values['total_cost'] *  $values['weight'];
-  $bname= $values['firstname'].' '.$values['lastname'];
-  
-
-
-
-
-?>
- <tr>
- <td><?php echo $itemid;?></td>
- <td><?php echo $vegname;?></td>
- <td><?php echo $pdate;?></td>
- <td><?php echo number_format($tweight,0).' kg'?></td>
- <td><?php echo number_format($cost,2);?></td>
- <td><?php echo $bname;?></td>
- 
- <td>
- <a class="more" name='link' onclick="" href="viewmore?id=<?php echo $ordid;?>">view more</a>
- </td>
-</tr>
-    
-
-<?php
-}}
-?>
+            foreach($data[0] as $key => $values){
+              $itemid= $values['item_id'];
+              $ordid= $values['order_id'];
+              $vegname= $values['vege_name'];
+              $pdate= $values['pickup_date'];
+              $tweight= $values['weight'];
+              $cost= $values['total_cost'] *  $values['weight'];
+              $bname= $values['firstname'].' '.$values['lastname'];
+              
 
 
 
-</table>
+
+            ?>
+            <tr>
+            <td><?php echo $itemid;?></td>
+            <td><?php echo $vegname;?></td>
+            <td><?php echo $pdate;?></td>
+            <td><?php echo number_format($tweight,0).' kg'?></td>
+            <td><?php echo number_format($cost,2);?></td>
+            <td><?php echo $bname;?></td>
+            
+            <td>
+            <a class="more" name='link' onclick="" href="viewmore?id=<?php echo $ordid;?>">view more</a>
+            </td>
+            </tr>
+          
+
+              <?php
+              }}
+              ?>
+
+
+
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <div class = "split right">
+
+          <h1 class="title2"> Farmer List </h2>
+            <div class="card">
+        
+              <div class="container2">
+                <?php
+                  foreach($data1 as $keys => $row){
+                    $farmername = $row['firstname']." ".$row['lastname'];
+                    $fid=$row['farmer_id'];?>
+                  <h3> <a class = "topic" href="public_profile?id=<?php echo $fid; ?>"><?php echo $farmername; ?></a> </h3>
+                <?php
+                  }
+                  ?>
+          
+              </div>
+            </div>
+      </div>
 </div>
-</div>
-</div>
-
-<div class = "split right">
-
-<h1 class="title2"> Farmer List </h2>
-<div class="card">
-  
-  <div class="container2">
-<?php
-  foreach($data1 as $keys => $row){
-    $farmername = $row['firstname']." ".$row['lastname'];
-    $fid=$row['farmer_id'];?>
-   <h3> <a class = "topic" href="public_profile?id=<?php echo $fid; ?>"><?php echo $farmername; ?></a> </h3>
-<?php
-  }
-  ?>
-    
-</div>
-</div>
-</div>
-
 <?php include("footer.php"); ?>
+
 </body>
 
 </html>
--- app/views/mentor/navbar_dash.php
+++ app/views/mentor/navbar_dash.php
@@ -9,11 +9,11 @@
 
 <div class="topnav" id="myTopnav">
   <a href="dash" class="navlogo"><img width=80px src="/thoga.lk/public/images/Farmer/logo1.png" alt="" class = "logo"></a>
-  <a href="dash" class="active">Dashboard</a>
-  <a href="view_price">Price List</a>
-  <a href="forum">Forum</a>
-  <a href="add_item">Add Item</a>
-  <a href="listed">Listed Items</a>
+  <a href="/thoa.lk/mentor/dash" class="active">Dashboard</a>
+  <a href="/thoa.lk/mentor/view_price">Price List</a>
+  <a href="/thoa.lk/mentor/forum">Forum</a>
+  <a href="/thoa.lk/mentor/add_item">Add Item</a>
+  <a href="/thoa.lk/mentor/listed">Listed Items</a>
   <a id="google_translate_element" ></a>
 
 
